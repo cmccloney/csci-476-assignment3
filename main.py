e96352414b56c949c447b9c02dca89da77a7ab79
Conner McCloney <cmccloney@hotmail.com>
2019-03-02 09:39:38 -0700

    finished

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import os
 import hashlib
 
-'''file = open("wordlist.txt","w")
-for i in range(0,200000):
-    file.write("%s:" % str(i))
-    obj = str(i)
-    file.write("%s\n" % hashlib.md5(obj.encode()).hexdigest())
-file.close()'''
-
-'''file = open("rockyou.txt","r",encoding="utf8")
-file_w = open("hashed_rock.txt","w",encoding="utf8")
+file = open("biglist.txt","r",encoding="utf8")
+file_w = open("hashed_biglist.txt","w",encoding="utf8")
 for i in file:
     string = i.split("\n")
     file_w.write("%s:" % string[0])
     file_w.write("%s\n" % hashlib.md5(string[0].encode()).hexdigest())
 file.close()
-file_w.close()'''
+file_w.close()
 
 passwords = open("passwords.txt","r")
 list = []
@@ -24,26 +17,21 @@ for y in passwords:
     list.append(string[0])
 passwords.close()
 
-dicti = open("wordlist.txt","r")
 cracked = open("cracked.txt","w")
-for x in dicti:
-    string = x.split(":")
-    string[1] = string[1].split("\n")
-    #print(string[1][0])
-    if string[1][0] in list:
-        #print(string[0])
-        cracked.write("%s:" % string[1][0])
-        cracked.write("%s\n" % string[0])
-            
-dicti.close()
-new_dict = open("hashed_rock.txt","r")
+new_dict = open("hashed_biglist.txt","r",encoding="utf8")
 for z in new_dict:
     string = z.split(":")
     string[1] = string[1].split("\n")
-    #print(string[1][0])
     if string[1][0] in list:
-        #print(string[0])
         cracked.write("%s:" % string[1][0])
         cracked.write("%s\n" % string[0])
 new_dict.close()
 cracked.close()
+
+list_pwd = open("cracked.txt","r")
+i = 0 #counter for preventing printing of duplicate cracked entries
+for entry in list_pwd:
+    if(i < 6):
+        print(entry)
+    i = i + 1
+list_pwd.close()
