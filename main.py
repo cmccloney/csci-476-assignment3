1726e6cfb8c84d9a80952f9602ee921b2bb72615
Conner McCloney <cmccloney@hotmail.com>
2019-03-02 10:06:48 -0700

    actually finished now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+import time
 
 file = open("biglist.txt","r",encoding="utf8")
 file_w = open("hashed_biglist.txt","w",encoding="utf8")
@@ -11,18 +12,25 @@ file.close()
 file_w.close()
 
 passwords = open("passwords.txt","r")
-list = []
+list_p = []
+timer = {}
 for y in passwords:
     string = y.split("\n")
-    list.append(string[0])
+    list_p.append(string[0])
+    timer[string[0]] = 0.0
 passwords.close()
 
 cracked = open("cracked.txt","w")
 new_dict = open("hashed_biglist.txt","r",encoding="utf8")
+t0 = time.time()
 for z in new_dict:
     string = z.split(":")
     string[1] = string[1].split("\n")
-    if string[1][0] in list:
+    if string[1][0] in list_p:
+        t1 = time.time() #used for tracking how long the program needed to
+        total = t1 - t0 #crack password
+        timer[string[1][0]] = total
+        t0 = t1
         cracked.write("%s:" % string[1][0])
         cracked.write("%s\n" % string[0])
 new_dict.close()
@@ -31,7 +39,9 @@ cracked.close()
 list_pwd = open("cracked.txt","r")
 i = 0 #counter for preventing printing of duplicate cracked entries
 for entry in list_pwd:
+    string = entry.split(":")
+    string[1] = string[1].split("\n")
     if(i < 6):
-        print(entry)
+        print("The password for hash value ", string[0], " is ", string[1][0], ", it takes the program %.3f sec to recover this password" % timer[string[0]])
     i = i + 1
 list_pwd.close()
